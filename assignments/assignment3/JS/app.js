87caa90bd989a82c43886ebb8304686ba8d05232
abdosso <abou_vip@hotmail.com>
2017-02-01 14:06:41 -0500

    Add files via upload

---
+++ assignments/assignment3/JS/app.js
@@ -0,0 +1,78 @@
+(function() {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+
+   
+
+   
+
+    NarrowItDownController.$inject = ['MenuSearchService', '$window'];
+    function NarrowItDownController(MenuSearchService, $window) {
+        var monControlleur = this;
+    
+        monControlleur.searchTerm = "";
+        monControlleur.found = [];
+        monControlleur.error = "";
+        monControlleur.foundControlleurResearch = function (search) {
+            if (search === undefined || search === "") {
+                monControlleur.found = [];
+                $window.alert("Please enter something to do a search ");
+            } else {
+                var promise = MenuSearchService.getMatchedMenuItems(search);
+                promise.then(function success(result) {
+                  
+                    if (result.length === 0) {
+                        monControlleur.found = [];
+                        $window.alert(" There is no corresponding match for your search !");
+                    }
+                    else { monControlleur.found = result; }
+                }, function (error) {
+                    if (error.status === 404) {
+                        $window.alert("  The page you were looking for doesn't exist (404) ");
+                    } else {
+                        $window.alert(" An error has occured : " + error.status + ":" + error.statusText);
+                    }
+                   
+                });
+            }
+           
+
+        };
+       
+        monControlleur.removeItem = function(indexToremove) {
+            monControlleur.found.splice(indexToremove, 1);
+        };
+
+    };
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+    function MenuSearchService($http, ApiBasePath) {
+
+        var service = this;
+        var found = [];
+        service.getMatchedMenuItems = function (term) {
+            function reussite(response) {
+                found = [];
+                angular.forEach(response.data.menu_items, function(element) {
+                    if (element.description.indexOf(term) !== -1) {
+                        found.push(element);
+                    }
+                });
+
+                return found;
+            };
+
+           return $http({ method: "GET", url: ApiBasePath })
+               .then(reussite);
+
+        };
+
+
+    }
+})();
---
+++ assignments/assignment3/JS/foundItems.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+    'use strict';
+    angular.module('NarrowItDownApp')
+           .directive('foundItems', FoundItemsDirective);
+
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItemList.html',
+            scope: {
+                found: '<',
+                onRemove: '&'
+            },
+            controller: foundListDirectiveController,
+            controllerAs: 'foundDirectiveControlleur',
+            bindToController: true
+        };
+        return ddo;
+
+    }
+    foundListDirectiveController.$inject = ['MenuSearchService'];
+    function foundListDirectiveController(MenuSearchService) {
+
+
+    };
+})();
