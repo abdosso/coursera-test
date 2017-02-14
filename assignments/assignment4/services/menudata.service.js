99a5092afa81d45f30d572021f345002e661fde3
abdosso <abou_vip@hotmail.com>
2017-02-14 15:31:58 -0500

    Add files via upload

---
+++ assignments/assignment4/services/menudata.service.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    angular.module('data')
+        .service('MenuDataService', MenuDataService)
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+
+    MenuDataService.$inject = ['$http', 'ApiBasePath'];
+    function MenuDataService($http, ApiBasePath) {
+        var myService = this;
+        myService.getAllCategories = function () {
+            function success(response) {
+           
+                    return response.data;
+            };
+
+
+            return $http({
+                method: "GET",
+                url: ApiBasePath + "/categories.json"
+            }).then(success);
+
+        };
+
+        myService.getItemsForCategory = function (categoryShortName) {
+            function reussite(response) {
+                
+                return response.data.menu_items;
+            };
+            
+            return $http({
+                method: "GET",
+                url: ApiBasePath + "/menu_items.json",
+                params : {
+                    category: categoryShortName
+                }
+            })
+            .then(reussite);
+
+        };
+    };
+
+   
+   
+})();
