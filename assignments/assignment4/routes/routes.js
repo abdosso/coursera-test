97aa9e9423af105f328494b8a3915364da92fad7
abdosso <abou_vip@hotmail.com>
2017-02-14 15:32:26 -0500

    Add files via upload

---
+++ assignments/assignment4/routes/routes.js
@@ -0,0 +1,41 @@
+﻿
+(function () {
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+        // Redirect to tab 1 if no other URL matches
+        $urlRouterProvider.otherwise('/');
+
+        // Set up UI states
+        $stateProvider
+             .state('/', {
+                 url: '/',
+                 templateUrl: 'home.html'
+             })
+          .state('categories', {
+              url: '/Categories',
+              templateUrl: 'src/templates/categories.html',
+              controller: 'CategorieListController as categorieCtrl',
+              resolve: {
+                  categoriesList: ['MenuDataService', function (MenuDataService) {
+                      return MenuDataService.getAllCategories();
+                  }]
+              }
+
+          })
+
+          .state('details', {
+              url: '/item-detail/{categoryForSearch}',
+              templateUrl: 'src/templates/items.html',
+              controller: 'itemsListController as itemsCtrl',
+              resolve: {
+                  itemsList: ['$stateParams','MenuDataService', function ($stateParams, MenuDataService) {
+                      return MenuDataService.getItemsForCategory($stateParams.categoryForSearch);
+                  }]
+              }
+          });
+    }
+})();
