a85e7c8b131c9eb19929ca7c326232a170017aab
abdosso <abou_vip@hotmail.com>
2017-02-14 15:43:12 -0500

    Add files via upload

---
+++ assignments/assignment4/src/menulist/components/items.component.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    angular.module('data')
+       .component('items', {
+           templateUrl: 'src/templates/items.template.html',
+         //  controller: ItemsComponentController ,
+           bindings: {
+               itemsListForComponent: '<'
+           }
+       });
+
+  
+})();
